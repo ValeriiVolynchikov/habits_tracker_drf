6ad9cd21fc0be7afe110720ccd918c7e4c3dc733
Alexandera Volynchikova <avoly@MacBook-Pro.local>
2025-07-30 12:49:02 +0300

    test

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from pathlib import Path
 
 from dotenv import load_dotenv
-gi
+
 load_dotenv(override=True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
